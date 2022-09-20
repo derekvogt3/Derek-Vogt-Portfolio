3ac82992431cadbb5e3fbe3377cd5cc65a15768b
Derek Vogt <derekvogt3@gmail.com>
2022-09-20 16:35:20 -0400

    minor tweaks

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -38,14 +38,14 @@ export default function ArticlesIndex({ articles }) {
   return (
     <>
       <Head>
-        <title>Articles - Spencer Sharp</title>
+        <title>Articles - Derek Vogt</title>
         <meta
           name="description"
           content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
         />
       </Head>
       <SimpleLayout
-        title="Writing on software design, company building, and the aerospace industry."
+        title="Writing on software development."
         intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
