66e30e833169197d5f701ed3d9b22e434642b84d
Derek Vogt <derekvogt3@gmail.com>
2022-10-19 11:56:49 -0400

    projects heading change

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -61,8 +61,8 @@ export default function Projects() {
         />
       </Head>
       <SimpleLayout
-        title="Things I’ve made trying to put my dent in the universe."
-        intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
+        title="Here are a few of my projects I'm most proud of."
+        intro="Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
       >
         <ul
           role="list"
